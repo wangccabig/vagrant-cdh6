6a9293a4675d1b5cec41e0dc835fdd99713e3a79
hanwl <hanwl@123.com>
2020-08-27 14:21:05 +0800

    fix bug

--- shell_node_install.sh
+++ shell_node_install.sh
@@ -8,10 +8,10 @@ server cdh-master  #<--该IP是NTP主机端的IP
 restrict cdh-master nomodify notrap noquery   # 允许上层时间服务器主动修改本机时间
 EOF
 
-ntpdate -u cdh-master				    # 强制同步时间
-service ntpd restart					# 重启时钟同步服务
-systemctl disable chronyd.service		# 禁用chronyd服务
-systemctl enable ntpd.service			# 开机启动
-ntpstat									# 查看同步状态
+ntpdate -u cdh-master
+service ntpd restart
+systemctl disable chronyd.service
+systemctl enable ntpd.service
+ntpstat
 
 # set ntdp ------------------end------------------
